494809fa3a62f85d6f621404a57a5bf7d63a9f62
sandiprb <sandip@coverfoxmail.com>
2016-10-18 01:10:43 +0530

    card-based views & cleaning

--- app/static/js/components/articles/Articles.jsx
+++ app/static/js/components/articles/Articles.jsx
@@ -10,17 +10,18 @@ class Article extends React.Component{
 		article = article[0]
 		return(
 			<article className="article clearfix">
-				<div className="wrapper col-sm-10 col-sm-offset-1 clearfix">
-					<div className="col-sm-3">
-						<img src={article.cover.thumbnail} alt={article.title} className="article-img" />
+				<div className="col-sm-10 col-sm-offset-1">
+					<div className="clearfix card_base list_wrapper">
+						<div className="col-sm-3">
+							<img src={article.cover.thumbnail} alt={article.title} className="article-img" />
+						</div>
+						<div className="col-sm-9">
+							<div className="category">{article.topic.title}</div>
+							<h4> <a href={article.permalink}> {article.title} </a> </h4>
+							<div className="author"> {article.authors[0].name} • {article.created} </div>
+							<p className="summary">{article.summary}</p>
+						</div>
 					</div>
-					<div className="col-sm-9">
-						<div className="category">{article.topic.title}</div>
-						<h4> <a href={article.permalink}> {article.title} </a> </h4>
-						<div className="author"> {article.authors[0].name} • {article.created} </div>
-						<p className="summary">{article.summary}</p>
-					</div>
-					<hr/>
 				</div>
 			</article>
 			)
